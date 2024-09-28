620e10ac637772f454c5e0d0bd2d2d49050f1553
Parth Sarthi Dubey <dubeysarthi12@gmail.com>
2024-09-28 23:37:24 +0530

    Auto login

--- Auto login and scraping/app.py
+++ Auto login and scraping/app.py
@@ -1,5 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+import time
 
 
 
@@ -14,14 +16,27 @@ def get_driver():
 
 
     driver = webdriver.Chrome(options=options)
-    driver.get("http://automated.pythonanywhere.com")
+    driver.get("https://automated.pythonanywhere.com/login/")
     return driver
 
+def text_cleaner(text):
+    output=float(text.split(": ")[1])
+    return output
 
 def main():
     driver = get_driver()
-    element = driver.find_element(by="xpath", value="/html/body/div[1]/div/h1[1]")
-    driver.find_element().send_keys()
-    return element.text
+
+    driver.find_element(by="id", value="id_username").send_keys("automated")
+    time.sleep(2)
+    driver.find_element(by="id", value="id_password").send_keys("automatedautomated" + Keys.RETURN)
+    time.sleep(2)
+
+    driver.find_element(by="xpath", value="/html/body/nav/div/a").click()
+    time.sleep(2)
+
+    element=driver.find_element(by="xpath", value="/html/body/div[1]/div/h1[2]").text
+    return text_cleaner(element)
+    
+    
 
 print(main())
